10ecd4f214fd78946d5b6cce16b036fe89f308b0
Mahesh Kumar <maheshkumar@BICSGLOBAL.COM>
2023-10-09 15:12:04 +0530

    mentione output statefile in subnetid's path

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ data "terraform_remote_state" "eks_vpc" {
   backend = "s3"
   config = {
     bucket         = "terra-ansi"
-    key            = "/statefile.tfstate"
+    key            = "backup/statefile.tfstate"
     region         = "ap-south-1"  # Change to your desired region
     encrypt        = true         # Enable encryption if needed
   }
